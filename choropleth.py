450773af43f087025b63f5cace0b5af3d20f777e
qchenevier <qchenevier@users.noreply.github.com>
2020-03-15 20:09:54 +0100

    init message

---
+++ choropleth.py
@@ -0,0 +1,39 @@
+import json
+import pandas as pd
+import folium
+import requests
+
+with open("new_geo.json") as f:
+    circonscriptions = json.load(f)
+
+#%%
+circonscriptions["features"][0]["properties"]
+
+#%%
+property_name_for_id = "nom"
+
+#%%
+ids = [c["properties"][property_name_for_id] for c in circonscriptions["features"]]
+
+#%%
+df = (
+    pd.DataFrame({"id": ids, "value": range(len(ids))})
+)
+
+#%%
+m = folium.Map(location=[43.6047, 1.4442], zoom_start=5)
+
+folium.Choropleth(
+    geo_data=circonscriptions,
+    data=df,
+    columns=["id", "value"],
+    key_on=f"properties.{property_name_for_id}",
+    legend_name="the legend",
+    fill_color='YlGn',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+).add_to(m)
+
+folium.LayerControl().add_to(m)
+
+m.save("plot.html")
